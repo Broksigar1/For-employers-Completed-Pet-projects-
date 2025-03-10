4cdd2a4c85fb74cbce858d30baa5508e4e2c0f62
Broksigar1 <cckirpra2@gmail.com>
2025-03-10 19:17:55 +0300

    Добавлена библиотека NUnit для тестирования

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -2,23 +2,9 @@
 {
     internal sealed class Program
     {
-        private static void Main()
+        static void Main()
         {
-            var list = new MyLinkedList<int>();
-            //Console.WriteLine(list.Count);
-            //list.AddFirst(1);
-            //list.AddFirst(2);
-            //list.AddFirst(3);
-            //Console.WriteLine(list.Count);
-            //list.RemoveFirst();
-            //list.RemoveFirst();
-            //list.RemoveFirst();
-            //Console.WriteLine(list.Count);
 
-            list.AddLast(1);
-            list.AddLast(2);
-            list.AddLast(3);
-            list.Clear();
         }
     }
 }
---
+++ LinkedList/Tests/MyLinkedListTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace LinkedList
+{
+    [TestFixture]
+    internal sealed class MyLinkedListTests
+    {
+        private bool CheckFirstNode<T>(MyLinkedList<T> list, T value)
+        {
+            if (list.Count == 1)
+                return list.First.Value.GetHashCode() == value.GetHashCode() && list.First.nextNode == list.First.previousNode == null;
+            else
+                return list.First.Value.GetHashCode() == value.GetHashCode() && list.First.nextNode != null && list.First.previousNode == null;
+        }
+
+        private bool CheckLastNode<T>(MyLinkedList<T> list, T value)
+        {
+            if (list.Count == 1)
+                return list.Last.Value.GetHashCode() == value.GetHashCode() && list.Last.nextNode == list.Last.previousNode == null;
+            else
+                return list.Last.Value.GetHashCode() == value.GetHashCode() && list.Last.previousNode != null && list.Last.nextNode == null;
+        }
+ 
+        [Test]
+        public void AddFirst()
+        {
+            var list = new MyLinkedList<int>();
+            var node = new Node<int>(1);
+            list.AddFirst(node);
+            Assert.That(CheckFirstNode<int>(list, 2), Is.EqualTo(true));
+        }
+    }
+}
